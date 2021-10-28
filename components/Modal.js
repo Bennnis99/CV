475624475a7c536082235cb395d90f4629e26289
Bennis99 <benjamin.kodehode@gmail.com>
2021-10-28 13:20:09 +0200

    k

--- components/Modal.js
+++ components/Modal.js
@@ -2,56 +2,31 @@ import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
 
-const Modal = ({ onClose, children, title }) => {
+const Modal = ({ show, onClose, children, title }) => {
     const [isBrowser, setIsBrowser] = useState(false);
-
-    // create ref for the StyledModalWrapper component
-    const modalWrapperRef = React.useRef();
-
-    // check if the user has clickedinside or outside the modal
-    const backDropHandler = e => {
-      if (!modalWrapperRef?.current?.contains(e.target)) {
-          onClose();
-      }
-    }
   
     useEffect(() => {
       setIsBrowser(true);
-
-     // attach event listener to the whole windor with our handler
-      window.addEventListener('click', backDropHandler);
-
-      // remove the event listener when the modal is closed
-      return () => window.removeEventListener('click', backDropHandler);
     }, []);
   
-    React.useLayoutEffect(() => {
-        console.log('here')
-    }, []);
-
     const handleCloseClick = (e) => {
       e.preventDefault();
       onClose();
     };
   
-    const modalContent = (
+    const modalContent = show ? (
       <StyledModalOverlay>
-        {/* Wrap the whole Modal inside the newly created StyledModalWrapper 
-            and use the ref
-        */}
-        <StyledModalWrapper ref={modalWrapperRef}>
-            <StyledModal>
-            <StyledModalHeader>
-                <a href="#" onClick={handleCloseClick}>
-                x
-                </a>
-            </StyledModalHeader>
-            {title && <StyledModalTitle>{title}</StyledModalTitle>}
-            <StyledModalBody>{children}</StyledModalBody>
-            </StyledModal>
-        </StyledModalWrapper>
+        <StyledModal>
+          <StyledModalHeader>
+            <a href="#" onClick={handleCloseClick}>
+              x
+            </a>
+          </StyledModalHeader>
+          {title && <StyledModalTitle>{title}</StyledModalTitle>}
+          <StyledModalBody>{children}</StyledModalBody>
+        </StyledModal>
       </StyledModalOverlay>
-    );
+    ) : null;
   
     if (isBrowser) {
       return ReactDOM.createPortal(
@@ -61,7 +36,7 @@ const Modal = ({ onClose, children, title }) => {
     } else {
       return null;
     }
-};
+  };
   
   const StyledModalBody = styled.div`
     padding-top: 10px;
@@ -73,20 +48,13 @@ const Modal = ({ onClose, children, title }) => {
     font-size: 25px;
   `;
   
-  // the wrapper component
-  const StyledModalWrapper = styled.div`
-    width: 500px;
-    height: 600px;
-    `;
-  
   const StyledModal = styled.div`
     background: white;
-    height:100%;
-    width:100%;
+    width: 60vw;
+    height: 50vh;
     border-radius: 15px;
-    padding: 15px;
+    padding: 2vw;
   `;
-
   const StyledModalOverlay = styled.div`
     position: absolute;
     top: 0;
@@ -99,4 +67,4 @@ const Modal = ({ onClose, children, title }) => {
     background-color: rgba(0, 0, 0, 0.5);
   `;
   
-export default Modal
+  export default Modal;
